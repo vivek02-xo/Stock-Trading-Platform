68395f1bb64aad7c0aea43af9dedf99e34d75b13
vivek02-xo <vivek02.xo@gmail.com>
2026-01-18 05:53:21 +0530

    Initial setup

---
+++ frontend/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <div>
+      <h1>Hellow!</h1>
+    </div>
+  </React.StrictMode>
+);
